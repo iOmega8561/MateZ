8073603cef3b27e87998b84dd3dfadf7232de934
Giuseppe <giusepperocco38@gmail.com>
2023-05-16 17:46:35 +0200

    More login work

--- MacroClient/Login.swift
+++ MacroClient/Login.swift
@@ -9,9 +9,74 @@ import SwiftUI
 
 struct Login: View {
     @StateObject var tempData: TempData
+    @Environment(\.colorScheme) var colorScheme
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
+    @State var isUnlocked: Bool = false
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack {
+                if colorScheme != .dark {
+                    Color(red: 0.949, green: 0.949, blue: 0.971).ignoresSafeArea()
+                    
+                }
+                
+                VStack {
+                    Spacer()
+                    
+                    Image("Joypad")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: 350.0, minHeight: 300.0)
+                    
+                    Text("🔫🔫 Log in 🔫🔫")
+                        .font(.title)
+                    
+                    Spacer()
+                    
+                    Form {
+                        
+                        Section(header: Text("Username")) {
+                            TextField("Type your username here", text: $username)
+                        }
+                        
+                        Section(header: Text("Password")) {
+                            SecureField("Type your password here", text: $password)
+                        }.onSubmit {
+                            isUnlocked.toggle()
+                        }
+                    }.frame(maxHeight: 300.0)
+                    
+                    Spacer()
+                    
+                    if !isUnlocked {
+                        Button(action: {}) {
+                            
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.white)
+                                .frame(width: 350, height: 50)
+                                .overlay(
+                                    Text("I don't have an account"))
+                            
+                        }.foregroundColor(.black)
+                        
+                    } else {
+                        NavigationLink(destination: MainView(tempData: tempData).navigationBarBackButtonHidden(true)) {
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.white)
+                                .frame(width: 350, height: 50)
+                                .overlay(
+                                    Text("Proceed to dashboard"))
+                        }.foregroundColor(.black)
+                    }
+                    
+                    Spacer()
+                }
+            }
+        }
     }
 }
 
