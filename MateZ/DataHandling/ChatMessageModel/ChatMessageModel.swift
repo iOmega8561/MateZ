a312b240b8e5f8745e32ed3e01fc68e516fdf409
Giuseppe <giusepperocco38@gmail.com>
2023-06-08 15:58:36 +0200

    Trying to fix this mess

---
+++ MateZ/DataHandling/ChatMessageModel/ChatMessageModel.swift
@@ -0,0 +1,20 @@
+//
+//  ChatMessageModel.swift
+//  MateZ
+//
+//  Created by Aryan Garg on 07/06/23.
+//
+
+import Foundation
+
+struct SubmittedChatMessage: Encodable {
+    let message: String
+    let user: String
+}
+
+struct ReceivingChatMessage: Decodable, Identifiable {
+    let date: Date
+    let id: UUID
+    let message: String
+    let user: String
+}
