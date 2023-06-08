de3a0d88bd3dd3e401a4723f25fe867917dc3ca9
Giuseppe <giusepperocco38@gmail.com>
2023-06-08 15:54:14 +0200

    Dudu

--- MateZ/DummyView.swift
+++ MateZ/DummyView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct DummyView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Work in progress ...")
     }
 }
 
